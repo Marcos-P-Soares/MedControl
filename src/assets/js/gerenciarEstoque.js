fb12a747ca6408ddc160536abe5bf7bdd3f06d36
Matheus da Silva <contagodzillajapan@gmail.com>
2024-01-25 17:12:00 -0300

    Verifica se há medicamentos na tabela

--- src/assets/js/gerenciarEstoque.js
+++ src/assets/js/gerenciarEstoque.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtém os medicamentos salvos do localStorage
-    const medicamentosSalvos = JSON.parse(localStorage.getItem('medicamentos')) || [];
+  // Obtém os medicamentos salvos do localStorage
+  const medicamentosSalvos = JSON.parse(localStorage.getItem('medicamentos')) || [];
 
-    // Obtém a tabela
-    const tabelaBody = document.getElementById('tabela-body');
+  // Obtém a tabela
+  const tabelaBody = document.getElementById('tabela-body');
 
-    // Preenche a tabela com os medicamentos salvos
-    medicamentosSalvos.forEach((medicamento) => {
-      const row = `
-        <tr>
-          <td>${medicamento.nome}</td>
-          <td>${medicamento.quantidade}</td>
-        </tr>
-      `;
-      tabelaBody.innerHTML += row;
-    });
-  });
+  // Verifica se há medicamentos na tabela
+  if (medicamentosSalvos.length === 0) {
+      tabelaBody.innerHTML = '<tr><td colspan="2">Não há medicamentos cadastrados</td></tr>';
+  } else {
+      // Preenche a tabela com os medicamentos salvos
+      medicamentosSalvos.forEach((medicamento) => {
+          const row = `
+              <tr>
+                  <td>${medicamento.nome}</td>
+                  <td>${medicamento.quantidade}</td>
+              </tr>
+          `;
+          tabelaBody.innerHTML += row;
+      });
+  }
+});
